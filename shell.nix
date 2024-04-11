3171d796f21077dba81b013390bbd1d1061afa34
Przemysław Kaminski <pk@intrepidus.pl>
2024-04-11 18:43:59 +0200

    [spago] use spago@next

--- shell.nix
+++ shell.nix
@@ -1,21 +1,27 @@
 let
-  pkgs = import <nixpkgs> {};
-  default-shell = import (
-    pkgs.fetchFromGitHub {
-      owner = "garganscript";
-      repo = "package-sets";
-      rev = "master";
-      sha256 = "1z7x5g0ba62l6hiagwsmnmdg07mz5xwf35qvb9p7fry2lks1ma18";
-    } + "/default-shell.nix");
+  nixpkgs = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.11.tar.gz";
+    #sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
+  };
+in
+
+{ pkgs ? import nixpkgs {} }:
+
+  let
+    ezPscSrc = pkgs.fetchFromGitHub {
+      owner = "justinwoo";
+      repo = "easy-purescript-nix";
+      rev = "117fd96acb69d7d1727df95b6fde9d8715e031fc";
+      sha256 = "lcIRIOFCdIWEGyKyG/tB4KvxM9zoWuBRDxW+T+mvIb0=";    };
+    ezPsc = import ezPscSrc { inherit pkgs; };
 in
 pkgs.mkShell {
   name = "purescript-reactix";
 
   buildInputs = [
-    default-shell.purs
-    default-shell.easy-ps.psc-package
-    default-shell.easy-ps.spago
-    default-shell.build
-    default-shell.pkgs.dhall-json
+    ezPsc.purs-0_15_15
+    ezPsc.psc-package
+    pkgs.nodejs
+    pkgs.yarn
   ];
 }
